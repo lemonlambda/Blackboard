21380d079f9a2b04efce280ff414eb3920686611
LemonjamesD <lemonjameslem@protonmail.com>
2022-10-04 19:57:53 -0400

    Added quotes back

--- src/command_builder.rs
+++ src/command_builder.rs
@@ -16,7 +16,7 @@ fn run (commands : Vec<String>, config : Config)
 		.replace("${linker}", &config.clone().tools.unwrap_or_default().linker.unwrap_or(String::from("clang")))
 		.replace("${src_files}", &format!(
 			"$({})", 
-			config.clone().meta.unwrap_or_default().src_files.unwrap_or(String::from("find ./src/ -name *.c"))
+			config.clone().meta.unwrap_or_default().src_files.unwrap_or(String::from("find ./src/ -name \"*.c\""))
 		))
 		.replace("${header_dirs}", &format!(
 			"$({})", 
@@ -24,7 +24,7 @@ fn run (commands : Vec<String>, config : Config)
 		))
 		.replace("${obj_files}", &format!(
 			"$({})", 
-			config.clone().meta.unwrap_or_default().obj_files.unwrap_or(String::from("find ./target/obj/ -name *.o"))
+			config.clone().meta.unwrap_or_default().obj_files.unwrap_or(String::from("find ./target/obj/ -name \"*.o\""))
 		))
 		.replace("${name}", &config.clone().package.name)
 		.replace("${version}", &config.clone().package.version)
