6dc82a9a38733d0568cbc448d0bce0276c3d13d9
LemonjamesD <lemonjameslem@protonmail.com>
2022-10-04 18:40:02 -0400

    Removed quotes

--- src/command_builder.rs
+++ src/command_builder.rs
@@ -14,16 +14,16 @@ fn run (commands : Vec<String>, config : Config)
 		.replace("${compiler}", &config.clone().tools.unwrap_or_default().compiler.unwrap_or(String::from("clang")))
 		.replace("${linker}", &config.clone().tools.unwrap_or_default().linker.unwrap_or(String::from("clang")))
 		.replace("${src_files}", &format!(
-			"\"$({})\"", 
-			config.clone().meta.unwrap_or_default().src_files.unwrap_or(String::from("find ./src/ -name \"*.c\""))
+			"$({})", 
+			config.clone().meta.unwrap_or_default().src_files.unwrap_or(String::from("find ./src/ -name *.c"))
 		))
 		.replace("${header_dirs}", &format!(
-			"\"$({})\"", 
+			"$({})", 
 			config.clone().meta.unwrap_or_default().header_dirs.unwrap_or(String::from("find ./src/include/ -type d"))
 		))
 		.replace("${obj_files}", &format!(
-			"\"$({})\"", 
-			config.clone().meta.unwrap_or_default().obj_files.unwrap_or(String::from("find ./target/obj/ -name \"*.o\""))
+			"$({})", 
+			config.clone().meta.unwrap_or_default().obj_files.unwrap_or(String::from("find ./target/obj/ -name *.o"))
 		))
 		.replace("${name}", &config.clone().package.name)
 		.replace("${version}", &config.clone().package.version)
