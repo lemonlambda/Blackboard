ce7b847a19f2be55ad9098002b59cfa3489a99ac
LemonjamesD <lemonjameslem@protonmail.com>
2022-10-04 16:18:29 -0400

    Added toml stuff

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+mod toml_format;
+
+use std::fs::read_to_string;
+
 use anyhow::{Result, Ok};
+use toml_format::Config;
 
 fn main () 
 -> Result<()> 
 {
-    
+    let contents = read_to_string("blackboard.toml")?;
+    let toml : Config = toml::from_str(&contents)?;
+
     Ok(())
 }
