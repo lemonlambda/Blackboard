3a30d0cd0810b3237dfd5738ffc765308a3db24c
LemonjamesD <lemonjameslem@protonmail.com>
2022-10-09 15:49:42 -0400

    Fixed Consts

--- src/toml_defaults.rs
+++ src/toml_defaults.rs
@@ -1,28 +1,26 @@
 use crate::toml_format::{Args, Bin, Compiling, Linking, Meta, Tools};
 
-const COMPILER: String = String::from("clang");
-const LINKER: String = String::from("clang");
+const COMPILER: &'static str = "clang";
+const LINKER: &'static str = "clang";
 
-const SRC_FILES: String = String::from("find ./src/ -name \"*.c\"");
-const HEADER_DIRS: String = String::from("find ./src/include -type d");
-const OBJ_FILES: String = String::from("find ./target/obj -name \"*.o\"");
+const SRC_FILES: &'static str = "find ./src/ -name \"*.c\"";
+const HEADER_DIRS: &'static str = "find ./src/include -type d";
+const OBJ_FILES: &'static str = "find ./target/obj -name \"*.o\"";
 
-const COMP_BEFORE: Vec<String> = vec![String::from("mkdir -p ./target/bin ./target/obj")];
-const COMP_RUN: Vec<String> = vec![String::from(
-    "${compiler} ${compiler_args} -c ${src_files} -I ${header_dirs}",
-)];
-const COMP_AFTER: Vec<String> = vec![String::from("mv *.o target/obj")];
+const COMP_BEFORE: [&'static str; 1] = ["mkdir -p ./target/bin ./target/obj"];
+const COMP_RUN: [&'static str; 1] =
+    ["${compiler} ${compiler_args} -c ${src_files} -I ${header_dirs}"];
+const COMP_AFTER: [&'static str; 1] = ["mv *.o target/obj"];
 
-const LINK_BEFORE: Vec<String> = vec![];
-const LINK_RUN: Vec<String> = vec![String::from(
-    "${compiler} ${linker_args} -B ${linker} ${obj_files} -o ${output_name}",
-)];
-const LINK_AFTER: Vec<String> = vec![String::from("rm -rf ./target/obj")];
+const LINK_BEFORE: [&'static str; 0] = [];
+const LINK_RUN: [&'static str; 1] =
+    ["${compiler} ${linker_args} -B ${linker} ${obj_files} -o ${output_name}"];
+const LINK_AFTER: [&'static str; 1] = ["rm -rf ./target/obj"];
 
-const COMP_ARGS: String = String::from("-O2");
-const LINK_ARGS: String = String::from("");
-const OUT_PATH: String = String::from("./target/bin");
-const OUT_NAME: String = String::from("${name}-${version}");
+const COMP_ARGS: &'static str = "-O2";
+const LINK_ARGS: &'static str = "";
+const OUT_PATH: &'static str = "./target/bin";
+const OUT_NAME: &'static str = "${name}-${version}";
 
 impl Default for Bin {
     fn default() -> Self {
@@ -41,8 +39,8 @@ impl Default for Bin {
 impl Default for Tools {
     fn default() -> Self {
         Self {
-            compiler: Some(COMPILER),
-            linker: Some(LINKER),
+            compiler: Some(COMPILER.to_string()),
+            linker: Some(LINKER.to_string()),
         }
     }
 }
@@ -50,9 +48,9 @@ impl Default for Tools {
 impl Default for Meta {
     fn default() -> Self {
         Self {
-            src_files: Some(SRC_FILES),
-            header_dirs: Some(HEADER_DIRS),
-            obj_files: Some(OBJ_FILES),
+            src_files: Some(SRC_FILES.to_string()),
+            header_dirs: Some(HEADER_DIRS.to_string()),
+            obj_files: Some(OBJ_FILES.to_string()),
         }
     }
 }
@@ -60,9 +58,27 @@ impl Default for Meta {
 impl Default for Compiling {
     fn default() -> Self {
         Self {
-            before: Some(COMP_BEFORE),
-            run: Some(COMP_RUN),
-            after: Some(COMP_AFTER),
+            before: Some(
+                COMP_BEFORE
+                    .to_vec()
+                    .into_iter()
+                    .map(|x| x.to_string())
+                    .collect(),
+            ),
+            run: Some(
+                COMP_RUN
+                    .to_vec()
+                    .into_iter()
+                    .map(|x| x.to_string())
+                    .collect(),
+            ),
+            after: Some(
+                COMP_AFTER
+                    .to_vec()
+                    .into_iter()
+                    .map(|x| x.to_string())
+                    .collect(),
+            ),
         }
     }
 }
@@ -70,9 +86,27 @@ impl Default for Compiling {
 impl Default for Linking {
     fn default() -> Self {
         Self {
-            before: Some(LINK_BEFORE),
-            run: Some(LINK_RUN),
-            after: Some(LINK_AFTER),
+            before: Some(
+                LINK_BEFORE
+                    .to_vec()
+                    .into_iter()
+                    .map(|x| x.to_string())
+                    .collect(),
+            ),
+            run: Some(
+                LINK_RUN
+                    .to_vec()
+                    .into_iter()
+                    .map(|x| x.to_string())
+                    .collect(),
+            ),
+            after: Some(
+                LINK_AFTER
+                    .to_vec()
+                    .into_iter()
+                    .map(|x| x.to_string())
+                    .collect(),
+            ),
         }
     }
 }
@@ -80,10 +114,10 @@ impl Default for Linking {
 impl Default for Args {
     fn default() -> Self {
         Self {
-            compiler_args: Some(COMP_ARGS),
-            linker_args: Some(LINK_ARGS),
-            out_name: Some(OUT_NAME),
-            out_path: Some(OUT_PATH),
+            compiler_args: Some(COMP_ARGS.to_string()),
+            linker_args: Some(LINK_ARGS.to_string()),
+            out_name: Some(OUT_NAME.to_string()),
+            out_path: Some(OUT_PATH.to_string()),
         }
     }
 }
