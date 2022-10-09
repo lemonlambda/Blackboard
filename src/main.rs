d5e8562608ab330f73652210d14bf1f571a392c2
LemonjamesD <lemonjameslem@protonmail.com>
2022-10-09 16:08:21 -0400

    Using CommandBuilder

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,9 @@ use std::{env, fs::read_to_string, path::Path};
 use anyhow::{Ok, Result};
 use clap::{command, Parser, Subcommand};
 use colored::Colorize;
-use command_builder::run;
 use toml_format::Config;
+
+use crate::command_builder::CommandBuilder;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None, propagate_version = true)]
@@ -21,6 +22,9 @@ struct Args {
 enum SubCommand {
     Test,
     Build {
+        #[clap(name = "BUILD_NAME", default_value = "")]
+        build_name: String,
+
         #[arg(short, long, default_value_t = false)]
         quiet: bool,
 
@@ -32,97 +36,46 @@ enum SubCommand {
 fn main() -> Result<()> {
     let args = Args::parse();
 
-    if let SubCommand::Build { quiet, path } = args.command {
+    if let SubCommand::Build {
+        build_name,
+        quiet,
+        path,
+    } = args.command
+    {
         let root = Path::new(&path);
         assert!(env::set_current_dir(&root).is_ok());
 
         let contents = read_to_string("blackboard.toml")?;
         let toml: Config = toml::from_str(&contents)?;
 
-        // Compiler
+        for x in toml.clone().bin {
+            if x.bin_name == build_name {
+                let builder = CommandBuilder::new(toml.clone(), x, quiet);
 
-        if !quiet {
-            println!("{}", "Compiling...".bright_magenta().bold());
+                // Compiler
+
+                if !quiet {
+                    println!("{}", "Compiling...".bright_magenta().bold());
+                }
+                builder.clone().comp_before()?;
+                builder.clone().comp_run()?;
+                builder.clone().comp_after()?;
+
+                // Linker
+
+                builder.clone().link_before()?;
+                builder.clone().link_run()?;
+                builder.clone().link_after()?;
+
+                if !quiet {
+                    println!("{}", "Linking...".bright_magenta().bold());
+                }
+
+                break;
+            }
         }
-
-        run(
-            toml.clone()
-                .compile
-                .unwrap_or_default()
-                .before
-                .unwrap_or(vec![String::from("mkdir -p ./target/obj ./target/bin")]),
-            toml.clone(),
-            "Before",
-            quiet,
-        )?;
-
-        run(
-            toml.clone()
-                .compile
-                .unwrap_or_default()
-                .run
-                .unwrap_or(vec![String::from(
-                    "${compiler} ${compiler_args} -c ${src_files} -I ${header_dirs}",
-                )]),
-            toml.clone(),
-            "Run",
-            quiet,
-        )?;
-
-        run(
-            toml.clone()
-                .compile
-                .unwrap_or_default()
-                .after
-                .unwrap_or(vec![String::from("mv *.o target/obj")]),
-            toml.clone(),
-            "After",
-            quiet,
-        )?;
-
-        // Linker
-
-        if !quiet {
-            println!("{}", "Linking...".bright_magenta().bold());
-        }
-
-        run(
-            toml.clone()
-                .linking
-                .unwrap_or_default()
-                .before
-                .unwrap_or(vec![]),
-            toml.clone(),
-            "Before",
-            quiet,
-        )?;
-
-        run(
-            toml.clone()
-                .linking
-                .unwrap_or_default()
-                .run
-                .unwrap_or(vec![String::from(
-                    "${compiler} ${linker_args} -B ${linker} ${obj_files} -o ${output_name}",
-                )]),
-            toml.clone(),
-            "Run",
-            quiet,
-        )?;
-
-        run(
-            toml.clone()
-                .linking
-                .unwrap_or_default()
-                .after
-                .unwrap_or(vec![String::from("rm -rf ./target/obj")]),
-            toml.clone(),
-            "After",
-            quiet,
-        )?;
-
-        Ok(())
     } else {
-        Ok(())
     }
+
+    Ok(())
 }
