75439e0886ad1293b2d1fb59596b183489eb582f
LemonjamesD <lemonjameslem@protonmail.com>
2022-10-09 13:29:36 -0400

    Fixed replaces

--- src/command_builder.rs
+++ src/command_builder.rs
@@ -1,35 +1,41 @@
 use std::process::Command;
 
-use crate::toml_format::Config;
+use crate::toml_format::{Bin, Config};
 
 use anyhow::{Ok, Result};
 use colored::Colorize;
 
-pub fn run(commands: Vec<String>, config: Config, stage: &str, quiet: bool) -> Result<()> {
+pub fn run(
+    commands: Vec<String>,
+    config: Config,
+    bin: Bin,
+    stage: &str,
+    quiet: bool,
+) -> Result<()> {
     let commands = commands.into_iter().map(|x| {
         let mut last = x.clone();
         loop {
             #[rustfmt::skip]
             let tmp = last
-                .replace("${output_name}", &config.clone().meta.unwrap_or_default().output_name.unwrap_or(String::from("./target/bin/${name}-${version}")))
-                .replace("${compiler}", &config.clone().tools.unwrap_or_default().compiler.unwrap_or(String::from("clang")))
-                .replace("${linker}", &config.clone().tools.unwrap_or_default().linker.unwrap_or(String::from("clang")))
+                .replace("${output_name}", &bin.clone().args.unwrap_or_default().out_name.unwrap_or(String::from("./target/bin/${name}-${version}")))
+                .replace("${compiler}", &bin.clone().tools.unwrap_or_default().compiler.unwrap_or(String::from("clang")))
+                .replace("${linker}", &bin.clone().tools.unwrap_or_default().linker.unwrap_or(String::from("clang")))
                 .replace("${src_files}", &format!(
                     "$({})", 
-                    config.clone().meta.unwrap_or_default().src_files.unwrap_or(String::from("find ./src/ -name \"*.c\""))
+                    bin.clone().meta.unwrap_or_default().src_files.unwrap_or(String::from("find ./src/ -name \"*.c\""))
                 ))
                 .replace("${header_dirs}", &format!(
                     "$({})", 
-                    config.clone().meta.unwrap_or_default().header_dirs.unwrap_or(String::from("find ./src/include/ -type d"))
+                    bin.clone().meta.unwrap_or_default().header_dirs.unwrap_or(String::from("find ./src/include/ -type d"))
                 ))
                 .replace("${obj_files}", &format!(
                     "$({})", 
-                    config.clone().meta.unwrap_or_default().obj_files.unwrap_or(String::from("find ./target/obj/ -name \"*.o\""))
+                    bin.clone().meta.unwrap_or_default().obj_files.unwrap_or(String::from("find ./target/obj/ -name \"*.o\""))
                 ))
                 .replace("${name}", &config.clone().package.name)
                 .replace("${version}", &config.clone().package.version)
-                .replace("${compiler_args}", &config.clone().meta.unwrap_or_default().compiler_args.unwrap_or(String::from("")))
-                .replace("${linker_args}", &config.clone().meta.unwrap_or_default().linker_args.unwrap_or(String::from("")));
+                .replace("${compiler_args}", &bin.clone().args.unwrap_or_default().compiler_args.unwrap_or(String::from("")))
+                .replace("${linker_args}", &bin.clone().args.unwrap_or_default().linker_args.unwrap_or(String::from("")));
             if last == tmp {
                 return tmp;
             }
