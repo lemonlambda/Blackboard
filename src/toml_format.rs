a0572ef7e23da571d986b5fc0d65d5fe0308a10e
LemonjamesD <lemonjameslem@protonmail.com>
2022-10-09 12:02:10 -0400

    Removed default

--- src/toml_format.rs
+++ src/toml_format.rs
@@ -21,34 +21,34 @@ pub struct Package {
     pub version: String,
 }
 
-#[derive(Deserialize, Clone, Debug, Default)]
+#[derive(Deserialize, Clone, Debug)]
 pub struct Tools {
     pub compiler: Option<String>,
     pub linker: Option<String>,
 }
 
-#[derive(Deserialize, Clone, Debug, Default)]
+#[derive(Deserialize, Clone, Debug)]
 pub struct Meta {
     pub src_files: Option<String>,
     pub header_dirs: Option<String>,
     pub obj_files: Option<String>,
 }
 
-#[derive(Deserialize, Clone, Debug, Default)]
+#[derive(Deserialize, Clone, Debug)]
 pub struct Args {
     pub compiler_args: Option<String>,
     pub linker_args: Option<String>,
     pub output_name: Option<String>,
 }
 
-#[derive(Deserialize, Clone, Debug, Default)]
+#[derive(Deserialize, Clone, Debug)]
 pub struct Compiling {
     pub before: Option<Vec<String>>,
     pub run: Option<Vec<String>>,
     pub after: Option<Vec<String>>,
 }
 
-#[derive(Deserialize, Clone, Debug, Default)]
+#[derive(Deserialize, Clone, Debug)]
 pub struct Linking {
     pub before: Option<Vec<String>>,
     pub run: Option<Vec<String>>,
